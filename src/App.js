d583c7871f639eefe07e77a72edfc4e4ab21b4bc
Denis <makedonski1995@abv.bg>
2020-07-12 22:04:32 +0300

    initial code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Rules from './Game/Rules';
+import Game from './Game/GameLogic';
+import landScape from './Bliss_(Windows_XP).png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppWrapper = styled.div`
+  padding: 1rem;
+  color: white;
+  text-align: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(${landScape});
+  height: 100vh;
+  box-sizing: border-box;
+  overflow: scroll;
+`
+const AppHeader = styled.h1`
+  display: inline-block;
+  background-color: rgba(0,0,0,0.5);
+  padding: 15px;
+`
+const AppLink = styled.a`
+  color: white;
+  background-color: rgba(0,0,0,0.5);
+  padding: 15px;
+  bottom: 0;
+  box-sizing: border-box;
+  display: inline-block;
+  margin: 2%;
+`
+
+const PlayAgainButton = styled.button`
+background: transparent;
+font-family: inherit;
+font-size: 14px;
+margin: 0 auto;
+font-weight: normal;
+color: white;
+padding: 1em;
+display: block;
+border: none;
+text-decoration: underline;
+cursor: pointer;
+`
+class App extends Component {
+  state = {
+    numberOfAttemptsSelected: 0
+  };
+
+  setNumberOfAttempts(numberOfAttemptsSelected) {
+    this.setState({ numberOfAttemptsSelected });
+  }
+
+  render() {
+    return (
+      <AppWrapper>
+        <AppHeader>Bulls and Cows game</AppHeader>
+        <Rules />
+        <PlayAgainButton onClick = { () => window.location.reload() }> 
+          PLAY AGAIN
+        </PlayAgainButton>
+        <Game 
+          numberOfAttemptsSelected = { this.state.numberOfAttemptsSelected } 
+        />
+        <AppLink href="https://github.com/rashmiap/bulls-cows-react-game">https://github.com/rashmiap/bulls-cows-react-game</AppLink>
+      </AppWrapper>
+    );
+  }
 }
 
 export default App;
